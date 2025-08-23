6dd67489137e0ef80f0205547bb6a234580f87e0
Thiago Pedro Padilha <thiago.pedro.padilha899@gmail.com>
2025-08-23 00:34:37 -0300

    CreateDbIfNotExists function

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -2,9 +2,9 @@
 {
     public class DbInitializer
     {
-        public async static void Initialize(ApplicationDbContext context)
+        public static void Initialize(ApplicationDbContext context)
         {
-            await context.Database.EnsureCreatedAsync();
+            context.Database.EnsureCreated();
         }
     }
 }
