41bc1647cc487f7648208d9d6f76eec0aef4baf3
411730 <411730@VDA-LCOII-19168>
2025-09-04 20:04:52 -0300

    Article data grid

--- EventManagement.Frontend/Program.cs
+++ EventManagement.Frontend/Program.cs
@@ -12,5 +12,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7041")
+});
 
 await builder.Build().RunAsync();
