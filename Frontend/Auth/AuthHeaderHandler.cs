34b573696c462f084bc86e6828c8eca7be864646
Thiago Pedro Padilha <thiago.pedro.padilha899@gmail.com>
2025-09-11 23:30:35 -0300

    AuthHeaderHanlder

--- Frontend/Auth/AuthHeaderHandler.cs
+++ Frontend/Auth/AuthHeaderHandler.cs
@@ -1,6 +1,13 @@
-﻿namespace Frontend.Auth
+﻿using System.Net.Http.Headers;
+using Blazored.LocalStorage;
+
+namespace Frontend.Auth
 {
-    public class AuthHeaderHandler
+    public class AuthHeaderHandler(ILocalStorageService localStorage) : DelegatingHandler
     {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpResponseMessage request, CancellationToken cancellationToken)
+        {
+            if (request.RequestUri.AbsolutePath)
+        }
     }
 }
--- Frontend/Auth/CustomAuthStateProvider.cs
+++ Frontend/Auth/CustomAuthStateProvider.cs
@@ -1,6 +1,90 @@
-﻿namespace Frontend.Auth
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Text.Json;
+using Blazored.LocalStorage;
+using System.Security.Claims;
+
+
+namespace Frontend.Auth
 {
-    public class CustomAuthStateProvider
+    public class CustomAuthStateProvider(ILocalStorageService localStorage) : AuthenticationStateProvider
     {
+        private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                var token = await localStorage.GetItemAsync<string>("authToken");
+
+                if (string.IsNullOrEmpty(token))
+                {
+                    return new AuthenticationState(_anonymous);
+                }
+
+                var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(token), "jwtAuth"));
+                return new AuthenticationState(claimsPrincipal);
+            }
+            catch
+            {
+                return new AuthenticationState(_anonymous);
+            }
+        }
+
+        public void NotifyUserAuthentication(string token)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(ParseClaimsFromJwt(token), "jwtAuth"));
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void NotifyUserLogout()
+        {
+            var authState = Task.FromResult(new AuthenticationState(_anonymous));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var claims = new List<Claim>();
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+
+            if (keyValuePairs != null)
+            {
+                keyValuePairs.TryGetValue(ClaimTypes.Role, out var roles);
+
+                if (roles != null)
+                {
+                    if (roles.ToString().Trim().StartsWith("["))
+                    {
+                        var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
+                        foreach (var parsedRole in parsedRoles)
+                        {
+                            claims.Add(new Claim(ClaimTypes.Role, parsedRole));
+                        }
+                    }
+                    else
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
+                    }
+                    keyValuePairs.Remove(ClaimTypes.Role);
+                }
+
+                claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
+            }
+
+            return claims;
+        }
+
+        private byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        }
     }
 }
